782b8f6db9ba6efafac3b34edc1f051eb7295683
Chloe <chloe.bajet@gmail.com>
2024-03-05 11:38:15 +0800

    prettier

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,21 +1,21 @@
 export const addTask = payload => ({
-    type: 'ADD_TASK',
+  type: 'ADD_TASK',
+  payload: payload,
+});
+
+export const removeTask = payload => {
+  return {
+    type: 'REMOVE_TASK',
     payload: payload,
-  });
-  
-  export const removeTask = payload => {
-    return {
-      type: 'REMOVE_TASK',
-      payload: payload,
-    };
   };
-  
-  export const markAsComplete = payload => ({
-    type: 'MARK_AS_COMPLETE',
-    payload: payload,
-  });
-  
-  export const filterTasks = payload => ({
-    type: 'FILTER_TASKS',
-    payload: payload,
-  });
+};
+
+export const markAsComplete = payload => ({
+  type: 'MARK_AS_COMPLETE',
+  payload: payload,
+});
+
+export const filterTasks = payload => ({
+  type: 'FILTER_TASKS',
+  payload: payload,
+});
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,50 +1,50 @@
 const INITIAL_STATE = {
-    tasks: [],
-    filter: 'All',
-  };
-  
-  let id = 0;
-  
-  const todoReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-      case 'ADD_TASK':
-        return {
-          ...state,
-          tasks: [
-            ...state.tasks,
-            {
-              name: action.payload.name,
-              priority: action.payload.priority,
-              isCompleted: action.payload.isCompleted,
-              id: id++,
-            },
-          ],
-        };
-      case 'REMOVE_TASK':
-        // console.log('task', action.payload);
-        return {
-          ...state,
-          tasks: state.tasks.filter(task => task.id !== action.payload),
-        };
-  
-      case 'MARK_AS_COMPLETE':
-        return {
-          ...state,
-          tasks: state.tasks.map(task =>
-            task.id === action.payload ? {...task, isCompleted: true} : task,
-          ),
-        };
-  
-      case 'FILTER_TASKS':
-        return {
-          ...state,
-          filter: action.payload,
-        };
-      case 'RESET_TODO':
-        return INITIAL_STATE;
-      default:
-        return state;
-    }
-  };
-  
-  export default todoReducer;
+  tasks: [],
+  filter: 'All',
+};
+
+let id = 0;
+
+const todoReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks: [
+          ...state.tasks,
+          {
+            name: action.payload.name,
+            priority: action.payload.priority,
+            isCompleted: action.payload.isCompleted,
+            id: id++,
+          },
+        ],
+      };
+    case 'REMOVE_TASK':
+      // console.log('task', action.payload);
+      return {
+        ...state,
+        tasks: state.tasks.filter(task => task.id !== action.payload),
+      };
+
+    case 'MARK_AS_COMPLETE':
+      return {
+        ...state,
+        tasks: state.tasks.map(task =>
+          task.id === action.payload ? {...task, isCompleted: true} : task,
+        ),
+      };
+
+    case 'FILTER_TASKS':
+      return {
+        ...state,
+        filter: action.payload,
+      };
+    case 'RESET_TODO':
+      return INITIAL_STATE;
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
